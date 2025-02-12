7b1023fd6aa68fa858f22e287337b374f5d9c90e
Mayara Bouwazra <mayarabouazra@gmail.com>
2025-02-12 09:15:45 +0100

    add command interface

---
+++ frontend_flutter/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'providers/commande_provider.dart';
+import 'views/add_commande_page.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CommandeProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Commande App',
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      supportedLocales: [
+        Locale('en', 'US'),  // Support for English
+        Locale('fr', 'FR'),  // Support for French
+      ],
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,  // Add localization delegate
+        GlobalWidgetsLocalizations.delegate,  // Add widget localization delegate
+        GlobalCupertinoLocalizations.delegate,  // Add Cupertino localization delegate
+      ],
+      home: AddCommandePage(),
+    );
+  }
+}
