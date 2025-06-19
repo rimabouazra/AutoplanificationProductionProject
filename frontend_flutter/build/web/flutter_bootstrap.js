9783615fff94cc88f355c670843a0ff0286df36d
Rima Bouazra <rima.bouazra.2003@gmail.com>
2025-06-19 19:47:14 +0100

    improve HomePage

--- frontend_flutter/build/web/flutter_bootstrap.js
+++ frontend_flutter/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1886895692"
+    serviceWorkerVersion: "42592042"
   }
 });
--- frontend_flutter/build/web/flutter_service_worker.js
+++ frontend_flutter/build/web/flutter_service_worker.js
@@ -58,14 +58,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "fba2957c059519f5219e448ae05dd044
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "85433ba545a7ff9c1b1baab63db1eb19",
+"flutter_bootstrap.js": "88aade813765e9c452aa384f70d71d59",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "6465dc9a008a1c115b9da4f931498a54",
 "/": "6465dc9a008a1c115b9da4f931498a54",
-"main.dart.js": "bf0289b20b2774b0f00f334e75df7725",
+"main.dart.js": "12a22556d993a45788c83f3a9d9ee95c",
 "manifest.json": "906fc433ff2f17c2c8af10ca573ca807",
 "version.json": "2b521e10dfa0f067561de489a19d6620"};
 // The application shell files that are downloaded before a service worker can
