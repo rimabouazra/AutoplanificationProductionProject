e1355e8365149381e7fc7d441b5384dc60a71d2e
Rima Bouazra <rima.bouazra.2003@gmail.com>
2025-08-04 22:58:05 +0100

    debugging

--- frontend_flutter/build/web/flutter_service_worker.js
+++ frontend_flutter/build/web/flutter_service_worker.js
@@ -58,7 +58,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "fba2957c059519f5219e448ae05dd044
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "29b19722bb822b1c0297600f1ae25ee5",
+"flutter_bootstrap.js": "d85aaa6c0be7c62d4009ab5cf939a241",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
